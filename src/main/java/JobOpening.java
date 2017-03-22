f2fdb4a07d4287d28ac6650d1cb06dfdddd93107
Chris Wainner and Mark Fisher <mark@>
2017-03-22 13:40:26 -0700

    Create JobOpening class and test

--- src/main/java/JobOpening.java
+++ src/main/java/JobOpening.java
@@ -1,3 +1,11 @@
 public class JobOpening {
+  private String mTitle;
+  private String mDescription;
+  private String mContact;
 
+  public JobOpening(String title, String description, String contact){
+    mTitle = title;
+    mDescription = description;
+    mContact = contact;
+  }
 }
--- src/test/java/JobOpeningTest.java
+++ src/test/java/JobOpeningTest.java
@@ -2,5 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class JobOpeningTest {
-
+  @Test
+  public void newJobOpening_instantiatesCorrectly_true(){
+    JobOpening testJobOpening = new JobOpening("test", "test", "999-999-9999");
+    assertEquals(true, testJobOpening instanceof JobOpening);
+  }
 }
